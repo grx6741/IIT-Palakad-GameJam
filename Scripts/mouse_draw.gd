3ee34a51152c3ea50af7adc4081b936691bb7d97
grx6741 <ikgowrish@gmail.com>
2024-01-21 15:50:39 +0530

    Ability to add RigidBodies to drawn Strokes has been implemented

--- Scripts/mouse_draw.gd
+++ Scripts/mouse_draw.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-var _lines : Array = []
+var _lines: Array = []
 
 func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_SPACE:
+			constructRigidBodies()
 	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if not _lines.is_empty() and _lines[-1] != null:
-			#print(_lines)
 			_lines.append(null)
 		return
 
@@ -16,8 +18,31 @@ func _input(event: InputEvent) -> void:
 			_lines.append(event.position)
 	queue_redraw()
 
+func get_null(array: Array, start: int = 0) -> int:
+	for i in range(start, len(array)):
+		if array[i] == null:
+			return i
+	return -1
+	
+func constructRigidBodies() -> void:
+	var i: int = 0
+	var j: int = get_null(_lines, i)
+	var _rigidBodies: Array = []
+
+	while i < len(_lines) and j > 0:
+		var line2d = Line2D.new()
+		while i < j:
+			line2d.add_point(_lines[i])
+			i += 1
+		var rigidBody = RigidBody2D.new()
+		rigidBody.add_child(line2d)
+		# add_child(rigidBody)
+		_rigidBodies.append(rigidBody)
+		i = j + 1
+		j = get_null(_lines, i)
+
 func _draw() -> void:
-	var i = 1
+	var i: int = 1
 	while i < len(_lines):
 		var prev = _lines[i-1]
 		var curr = _lines[i]
