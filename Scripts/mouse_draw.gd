e2998d1edacd37370cad27ef1280cfe447c1858f
grx6741 <ikgowrish@gmail.com>
2024-01-26 09:44:55 +0530

    Minor Bugfixes

--- Scripts/mouse_draw.gd
+++ Scripts/mouse_draw.gd
@@ -1,5 +1,11 @@
 extends "res://Scripts/create_colliders.gd"
 
+enum StrokeMode {
+	NORMAL_STROKE,
+	ERASER_STROKE,
+	JOINT_STROKE,
+}
+
 var _lines: Array = []
 var _rigidBodies: Array = []
 
@@ -47,7 +53,7 @@ func _input(event: InputEvent) -> void:
 		else:
 			if _lines[-1] != event.position:
 				_lines.append(event.position)
-		queue_redraw()
+		# queue_redraw()
 
 func constructRigidBodies() -> void:
 	var i: int = 0
@@ -56,14 +62,13 @@ func constructRigidBodies() -> void:
 	while j > 0:
 		var line2d: Line2D = Line2D.new()
 		while i < j:
-			line2d.add_point(_lines[i])
+			line2d.add_point(_lines[i] - line2d.global_position)
 			line2d.width = 3
 			i += 1
 		if line2d.get_point_count() < 1:
 			continue
 		var rigidBody = RigidBody2D.new()
 		handleColliderCreation(rigidBody, line2d.points)
-		print(1, " ", len(line2d.points))
 		rigidBody.add_child(line2d)
 		rigidBody.mass = len(line2d.points)
 		# rigidBody.kine
@@ -71,6 +76,12 @@ func constructRigidBodies() -> void:
 		_rigidBodies.append(rigidBody)
 		i = j + 1
 		j = get_null(_lines, i)
+
+func in_between_x(a: Vector2, b:Vector2, c: Vector2) -> bool:
+	return ( a.x <= c.x and c.x <= b.x ) or ( b.x <= c.x and c.x <= b.x )
+
+func in_between_y(a: Vector2, b:Vector2, c: Vector2) -> bool:
+	return ( a.y <= c.y and c.y <= b.y ) or ( b.y <= c.y and c.y <= b.y )
 
 func _draw() -> void:
 	if len(_lines) > 1:
@@ -85,3 +96,22 @@ func _draw() -> void:
 			draw_circle(prev, 5, Color.BLACK)
 			draw_circle(curr, 5, Color.BLACK)
 			i += 1
+	if _optionNode.get_selected_id() == StrokeMode.JOINT_STROKE:
+		var mouse_pos: Vector2 = get_global_mouse_position()
+		# print(mouse_pos)
+		if _rigidBodies.is_empty():
+			return
+		for rb in _rigidBodies:
+			var children: Array = rb.get_children()
+			for i in range(1, children.size()):
+				if children[i] is CollisionShape2D:
+					var prev_pos = children[i-1].global_position
+					var curr_pos = children[i].global_position
+
+					if (in_between_x(prev_pos, curr_pos, mouse_pos)):
+						draw_circle(Vector2(mouse_pos.x, 0.5 * (prev_pos.y + curr_pos.y)), 10, Color.RED)
+					if (in_between_y(prev_pos, curr_pos, mouse_pos)):
+						draw_circle(Vector2(0.5 * (prev_pos.x + curr_pos.x), mouse_pos.y), 10, Color.RED)
+
+func _process(delta: float) -> void:
+	queue_redraw()
