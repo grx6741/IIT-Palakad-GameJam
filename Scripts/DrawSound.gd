9225c05fa891c0c7dc83324e59e09bcbe7eacabb
ElectroCubic <anushbundel26@gmail.com>
2024-02-01 19:36:31 +0530

    Submittable version 0.5

--- Scripts/DrawSound.gd
+++ Scripts/DrawSound.gd
@@ -8,7 +8,6 @@ func _on_player_interface__signal_draw_audio():
 	_audio_player.pitch_scale = randf()/4.0 + 1.0
 	if not _audio_player.playing:
 		_audio_player.play()
-		print("Playing")
 
 
 func _on_player_interface__signal_not_draw_audio():
--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,12 +1,16 @@
 extends Node
 
-var game_paused: bool
+class_name GameManager
 
-func _ready():
-	$Pause_Menu.visible = false
+signal toggle_game_pause(is_paused : bool)
 
-func _input(event: InputEvent):
-	if Input.is_action_pressed("ui_cancel"):
-		game_paused = get_tree().paused
-		game_paused = not game_paused
-		$Pause_Menu.visible = not $Pause_Menu.visible
+var game_paused: bool = false :
+	get:
+		return game_paused
+	set(value):
+		game_paused = value
+		get_tree().paused = not game_paused
+		emit_signal("toggle_game_pause", game_paused)
+
+func _on_player_interface_paused_btn():
+	game_paused = not game_paused
